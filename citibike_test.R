2bb0d0867553d5628ddecaaf397fec2ccb336ef6
Benjamin Noland <benjaminnoland93@gmail.com>
2016-09-20 00:09:48 -0400

    Added citibike_test.R

--- citibike_test.R
+++ citibike_test.R
@@ -4,21 +4,34 @@
 
 library(docopt)
 
+TimeSample <- function(start.time, end.time, n) {
+    # Returns a vector of times uniformly distributed between two given times.
+    #
+    # Args:
+    #   start.time: The start time.
+    #   end.time: The end time.
+    #   n: The number of times to generate.
+    #
+    # Returns:
+    #   A vector containing the times generated.
+    
+    dt <- as.numeric(difftime(end.time, start.time, unit="sec"))
+    offsets <- runif(n, 0, dt)
+    return(start.time + offsets)
+}
+
 # Process the command line arguments.
 
-"Usage: citibike_test.R (--nobs N) (--nstations N) [--time-beg TIME] [--time-end TIME] [--trip-dur-mean M] [--trip-dur-sd S] [--granularity G]
+"Usage: citibike_test.R (--out-file FILE) (--nobs N) (--nstations N) [--min-start-time TIME] [--max-start-time TIME] [--min-trip-duration D] [--max-trip-duration D]
 
---help              Show this.
---nobs N            Specify the number of observations to generate.
---nstations N       Specify the number of stations.
---time-beg TIME     Specify the beginning of the start time range.
---time-end TIME     Specify the end of the start time range.
---granularity G     Specify the minimum time between the start times of any two observations (e.g., \"1 secs\").
---trip-dur-mean M   Specify trip duration mean.
---trip-dur-sd S     Specify trip duration standard deviation
-
-Values for the trip duration are assumed to be normally distributed with given mean and
-standard deviation.
+--help                  Show this.
+--out-file FILE         Specify output file.
+--nobs N                Specify number of observations to generate.
+--nstations N           Specify number of stations.
+--min-start-time TIME   Specify minimum starting time.
+--max-start-time TIME   Specify maximum starting time.
+--min-trip-duration D   Specify minimum trip duration (in seconds).
+--max-trip-duration D   Specify maximum trip duration (in seconds).
 
 The values for --time-beg and --time-end are to be specified in the following R date/time
 format: \"%m/%d/%Y %H:%M:%S\" (e.g., \"9/1/2013 1:03:12\", \"12/24/2014 23:25:00\")." -> doc
@@ -27,25 +40,47 @@ options <- docopt(doc)
 
 kTimeFormat <- "%m/%d/%Y %H:%M:%S"
 
+out.file <- options[["out-file"]]
 nobs <- as.integer(options[["nobs"]])
 nstations <- as.integer(options[["nstations"]])
 
-time.beg <- strptime("1/1/2000 00:00:00", format=kTimeFormat)
-if (!is.null(options[["time-beg"]]))
-    time.beg <- strptime(options[["time-beg"]], format=kTimeFormat)
+# TODO: For some reason the ifelse statement is vomiting a warning when I use it to process the time
+# arguments. Look into this.
 
-time.end <- strptime("1/31/2000 23:59:59", format=kTimeFormat)
-if (!is.null(options[["time-end"]]))
-    time.end <- strptime(options[["time-end"]], format=kTimeFormat)
+if (!is.null(options[["min-start-time"]])) {
+    min.start <- strptime(options[["min-start-time"]], format=kTimeFormat)
+} else {
+    min.start <- strptime("01/01/2000 00:00:00", format=kTimeFormat)
+}
 
-#granularity <- ifelse(is.null(options[["granularity"]]), "1 secs", options[["granularity"]])
+if (!is.null(options[["max-start-time"]])) {
+    max.start <- strptime(options[["max-start-time"]], format=kTimeFormat)
+} else {
+    max.start <- strptime("01/31/2000 23:59:59", format=kTimeFormat)
+}
 
-# Default values for mean and stddev based on values from 6/2016 data.
-trip.dur.mean <- ifelse(is.null(options[["trip-dur-mean"]]), 1000, as.integer(options[["trip-dur-mean"]]))
-trip.dur.sd <- ifelse(is.null(options[["trip-dur-sd"]]), 7000, as.integer(options[["trip-dur-sd"]]))
+# TODO: Make sure min.start <= max.start?
 
-print(time.beg)
-print(time.end)
+min.trip <- ifelse(is.null(options[["min-trip-duration"]]), 0, as.integer(options[["min-trip-duration"]]))
+max.trip <- ifelse(is.null(options[["max-trip-duration"]]), 0, as.integer(options[["max-trip-duration"]]))
 
-print(trip.dur.mean)
-print(trip.dur.sd)
+# TODO: Make sure min.trip <= max.trip?
+
+# Initialize the relevant attributes.
+
+tripduration <- sample(min.trip:max.trip, size=nobs, replace=TRUE)
+starttime <- TimeSample(min.start, max.start, nobs)
+stoptime <- starttime + tripduration
+
+# Make the times strings with the correct formatting.
+starttime <- format(starttime, format=kTimeFormat)
+stoptime <- format(stoptime, format=kTimeFormat)
+
+start.station.id <- sample(1:nstations, size=nobs, replace=TRUE)
+end.station.id <- sample(1:nstations, size=nobs, replace=TRUE)
+
+# Shove everything into a data frame.
+citibike <- data.frame(tripduration, starttime, stoptime, start.station.id, end.station.id)
+
+# Write the data into the specified file.
+write.csv(citibike, out.file, row.names=FALSE)
