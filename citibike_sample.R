924a7ac52cb4a6563e526ba9dca5141af255c7f3
Benjamin Noland <benjaminnoland93@gmail.com>
2016-09-22 02:37:51 -0400

    Removed --clean option from citibike_sample.R

--- citibike_sample.R
+++ citibike_sample.R
@@ -7,7 +7,7 @@ library(docopt)
 
 # Process the command line arguments.
 
-"Usage: citibike_sample.R (--in-file FILE) (--out-file FILE) (--size N) [--nrows N] [--replace] [--clean] [--row-names]
+"Usage: citibike_sample.R (--in-file FILE) (--out-file FILE) (--size N) [--nrows N] [--replace] [--row-names]
 
 --help          Show this.
 --in-file FILE  Specify input file.
@@ -15,7 +15,6 @@ library(docopt)
 --size N        Specify sample size.
 --nrows N       Specify number of rows to read from the start of the data file.
 --replace       Take samples with replacement; default is without replacement.
---clean         Remove all attributes not used by the grouping methods (removes clutter for testing).
 --row-names     Output row names." -> doc
 
 options <- docopt(doc)
@@ -36,21 +35,6 @@ if (!replace && size > nrow(citibike))
 
 # Extract a sample as specified.
 samp <- citibike[sample(nrow(citibike), size, replace), ]
-
-# If specified, clear out all the attributes not used to group the observations.
-if (clean) {
-    samp$tripduration <- NULL
-    samp$start.station.name <- NULL
-    samp$start.station.latitude <- NULL
-    samp$start.station.longitude <- NULL
-    samp$end.station.name <- NULL
-    samp$end.station.latitude <- NULL
-    samp$end.station.longitude <- NULL
-    samp$bikeid <- NULL
-    samp$usertype <- NULL
-    samp$birth.year <- NULL
-    samp$gender <- NULL
-}
 
 # Write the sample out to the specified file.
 write.csv(samp, out.file, row.names=row.names)
