eea823a6afb9ebf12a178acb906192b4c07b11a1
Benjamin Noland <benjaminnoland93@gmail.com>
2016-10-17 19:27:19 -0400

    Looking better...

--- gen_groups.bat
+++ gen_groups.bat
@@ -3,9 +3,9 @@
 set datadir=data
 set groupeddir=grouped
 
-del /s /q %groupeddir%
+del /s /q %groupeddir%\*.csv
 
-for %%i in (%datadir%/*) do (
+for %%i in (%datadir%/*.csv) do (
     Rscript citibike_group.R --in-file %datadir%/%%i --out-file %groupeddir%/method1/%%i --method 1 --show-progress
     Rscript citibike_group.R --in-file %datadir%/%%i --out-file %groupeddir%/method2/%%i --method 2 --show-progress
 )
--- gen_stats.bat
+++ gen_stats.bat
@@ -5,12 +5,12 @@ set statsdir=stats
 
 set datayear=2016
 
-del /s /q %statsdir%
+del /s /q %statsdir%\*.txt
 
-for %%i in (%groupeddir%/method1/*) do (
+for %%i in (%groupeddir%/method1/*.csv) do (
     Rscript citibike_stats.R --in-file %groupeddir%/method1/%%i --out-file %statsdir%/method1/%%~ni.txt --data-year %datayear%
 )
 
-for %%i in (%groupeddir%/method2/*) do (
+for %%i in (%groupeddir%/method2/*.csv) do (
     Rscript citibike_stats.R --in-file %groupeddir%/method2/%%i --out-file %statsdir%/method2/%%~ni.txt --data-year %datayear%
 )
